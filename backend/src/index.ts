23d5d5b6305cd11ab38095d69b33c28dd6f73be3
Razeen <77452515+Razeen2004@users.noreply.github.com>
2025-04-25 22:42:03 +0500

    some changes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,28 +2,24 @@
 import express from 'express'
 import cors from 'cors'
 import authRoutes from './routes/authRoutes'
-import dotenv from 'dotenv'
-import SendMail from './utils/mail'
+import dotenv from 'dotenv';
+
 const app = express()
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
     credentials: true
 }));
 
 
 dotenv.config()
-
-const port = process.env.PORT || 5000;
+const port = Number(process.env.PORT) || 5000;
 
 // Routes
 
 app.use('/api/auth', authRoutes);
 
-// SendMail("razeenbaig10@gmail.com", "Test Email", "This is a test email from Node.js");
-
-
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`Server running on port ${port}`)
 })
