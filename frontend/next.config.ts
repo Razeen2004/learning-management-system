c65c582b195ca175f2e55c732496cfef4a89685d
Razeen <77452515+Razeen2004@users.noreply.github.com>
2025-04-26 00:31:09 +0500

    changes

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  env: {
+    NODE_TLS_REJECT_UNAUTHORIZED: '0',
+  },
   output: "standalone",
   images: {
     remotePatterns: [
