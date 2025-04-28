0a07dd89f1846ac09cdf5d436148512503e0f283
Razeen <77452515+Razeen2004@users.noreply.github.com>
2025-04-28 08:30:00 +0500

    Some changes in dashboard and home page

--- frontend/components/team-switcher.tsx
+++ frontend/components/team-switcher.tsx
@@ -39,7 +39,7 @@ export function TeamSwitcher() {
             </div>
             <div className="grid flex-1 text-left text-sm leading-tight">
               <span className="truncate font-semibold">
-                {"Long Learning"}
+                {"Nexlify"}
               </span>
               <span className="truncate text-xs">{"Amplify Your Learning Journey"}</span>
             </div>
